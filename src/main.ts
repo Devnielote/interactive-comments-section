147280dbc24599ccd89d278c4d5ef73262d5e8d8
Devnielote <danielfc_26@hotmail.com>
2022-09-05 11:32:32 -0500

    fn for create, update and delete work fine

--- src/main.ts
+++ src/main.ts
@@ -27,3 +27,42 @@ import { createComment, updateComment, replyToComment, DeleteComment, existingCo
 // });
 
 //TODO: Definir la interface de user que recibira la interface de Comment
+const loadComments = () => {
+  data.comments.forEach(comment => {
+    createComment(comment);
+  })
+}
+
+
+loadComments();
+createComment(
+  {id: 5,
+  comment: 'Hello there, just wanted to say: aquí es pizzas castillo?',
+  createdAt: '1 minute ago',
+  score: 10,
+  user:{
+  "image": {
+    "png": "./images/avatars/image-amyrobson.png",
+    "webp": "./images/avatars/image-amyrobson.webp"
+  },
+  "username": "Daniel Flores"
+},
+replies: []
+})
+updateComment(1,'Lol, u gay');
+replyToComment(existingComments[0].id,
+  {
+    id: 1.1,
+    comment: "Yo, shut the fuck up",
+    createdAt: "2 weeks ago",
+    score: 10,
+    user: {
+      "image": {
+        "png": "./images/avatars/image-amyrobson.png",
+        "webp": "./images/avatars/image-amyrobson.webp"
+      },
+      "username": "Daniel Flores"
+    },
+  })
+  console.log(existingComments);
+

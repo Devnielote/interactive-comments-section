48e551d8d128ed35baf60dc8016d71cd3779ff4f
Devnielote <danielfc_26@hotmail.com>
2023-02-24 11:49:52 -0600

    Fix the issue where the generated accounts wouldn't be able to use its methods due to the fact that they where just a copy of the data fetched from the local storage

--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -3,11 +3,13 @@ import { Account } from "./accounts/Account";
 import { Comment, CommentTypeEnum } from "./comments/comment.model";
 
 export let users: Account[];
-export let updatedUsers: Account[];
+export let currentUserV2: Account;
+
 let initialUsers = getAccountsFromStorage();
 
 if(initialUsers){
-  users = [...initialUsers];
+  users = generateAccountsFromLocalStorage(initialUsers); //Debo crear nuevos accounts y sus comentarios de lo que exista en el localStorage de otra forma no se instancian las clases y solo son copias sin metodos heredados.
+  currentUserV2 = users[4];
 } else {
   initialUsers = createInitialAccounts()
 }
@@ -27,7 +29,9 @@ function createInitialAccounts():Account[] {
       account.createComment(comment);
       initialAccounts.push(account);
     }
-
+    currentUserV2 = new Account;
+    currentUserV2.name = 'You';
+    initialAccounts.push(currentUserV2);
     localStorage.setItem('accounts', JSON.stringify(initialAccounts));
     return initialAccounts;
 };
@@ -40,4 +44,16 @@ export function getAccountsFromStorage(): Account[]  {
 export function setAccountsToStorage(account: Account []): void {
   localStorage.setItem('accounts', JSON.stringify(account));
 };
+
+export function updateCommentsInLocalStorage(accounts: Account[]): boolean{
+  return true;
+};
+
+function generateAccountsFromLocalStorage( array: Account[]): Account[] {
+  let newAccounts: Account[] = [];
+  array.map(acc => {
+    newAccounts.push(new Account(acc))
+  })
+  return newAccounts;
+}
 
